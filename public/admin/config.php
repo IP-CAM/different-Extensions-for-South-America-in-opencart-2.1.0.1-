b32adb955eeb23d20bdd5c616471acba845c8885
Marco Foganholo <marco@lojavirtual.digital>
2016-07-18 19:32:48 -0300

    Up Files

---
+++ public/admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://opencart57564.lojavirtual.local/admin/');
+define('HTTP_CATALOG', 'http://opencart57564.lojavirtual.local/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://opencart57564.lojavirtual.local/admin/');
+define('HTTPS_CATALOG', 'http://opencart57564.lojavirtual.local/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/Users/mfoganholo/Documents/GitHub/opencart57564/public/admin/');
+define('DIR_SYSTEM', 'C:/Users/mfoganholo/Documents/GitHub/opencart57564/public/system/');
+define('DIR_LANGUAGE', 'C:/Users/mfoganholo/Documents/GitHub/opencart57564/public/admin/language/');
+define('DIR_TEMPLATE', 'C:/Users/mfoganholo/Documents/GitHub/opencart57564/public/admin/view/template/');
+define('DIR_CONFIG', 'C:/Users/mfoganholo/Documents/GitHub/opencart57564/public/system/config/');
+define('DIR_IMAGE', 'C:/Users/mfoganholo/Documents/GitHub/opencart57564/public/image/');
+define('DIR_CACHE', 'C:/Users/mfoganholo/Documents/GitHub/opencart57564/public/system/storage/cache/');
+define('DIR_DOWNLOAD', 'C:/Users/mfoganholo/Documents/GitHub/opencart57564/public/system/storage/download/');
+define('DIR_LOGS', 'C:/Users/mfoganholo/Documents/GitHub/opencart57564/public/system/storage/logs/');
+define('DIR_MODIFICATION', 'C:/Users/mfoganholo/Documents/GitHub/opencart57564/public/system/storage/modification/');
+define('DIR_UPLOAD', 'C:/Users/mfoganholo/Documents/GitHub/opencart57564/public/system/storage/upload/');
+define('DIR_CATALOG', 'C:/Users/mfoganholo/Documents/GitHub/opencart57564/public/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'root');
+define('DB_DATABASE', 'opencart57564');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
